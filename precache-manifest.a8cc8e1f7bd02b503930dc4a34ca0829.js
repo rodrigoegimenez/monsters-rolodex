f54552e482bc6bdf157b1e2d08ca0f3323a5d569
Rodrigo Gimenez <rodrigog83@gmail.com>
2020-08-09 16:54:25 +0200

    Updates

---
+++ precache-manifest.a8cc8e1f7bd02b503930dc4a34ca0829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a790ce4ac288a26e39c084f05abb3421",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88e846f1f2c075445cf3",
+    "url": "/monsters-rolodex/static/css/main.a11e3d62.chunk.css"
+  },
+  {
+    "revision": "ed9f4125c6ddc10185a3",
+    "url": "/monsters-rolodex/static/js/2.a281a20f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a281a20f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e846f1f2c075445cf3",
+    "url": "/monsters-rolodex/static/js/main.e8b3fc75.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
